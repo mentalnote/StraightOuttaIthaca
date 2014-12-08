044edec6210c038f79e0d57ad3924b36b613ab9a
Leonie O'Connor <leonieoconnor@2bitstudios.com.au>
2014-12-08 14:07:43 +1000

    Added cyclone and some lift logic

--- Assets/Scripts/God/LiftLogic.cs
+++ Assets/Scripts/God/LiftLogic.cs
@@ -1,15 +1,63 @@
 ﻿using UnityEngine;
-using System.Collections;
 
-public class LiftLogic : MonoBehaviour {
+public class LiftLogic : MonoBehaviour 
+{
+    private SphereCollider sphereCollider;
+    private Transform selectedFollower = null;
+    private float liftHeight = 52;  //max height of terrain
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    private void Start()
+    {
+        sphereCollider = collider as SphereCollider;
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, sphereCollider.radius);
+
+        float smallestDistance = 0;
+
+        for (int i = 0; i < hitColliders.Length; ++i)
+        {
+            if (hitColliders[i].name.StartsWith("Follower"))
+            {
+                Transform follower = hitColliders[i].gameObject.transform;
+                float distanceFromLiftPt = (follower.position - sphereCollider.transform.position).magnitude;
+                if (selectedFollower == null || distanceFromLiftPt < smallestDistance)
+                {
+                    selectedFollower = follower;
+                }
+            }
+        }
+
+        Debug.Log("follower = " + selectedFollower);
+    }
+
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePt = camera.ScreenToWorldPoint(Input.mousePosition);
+
+            selectedFollower.position = new Vector3(mousePt.x, liftHeight, mousePt.z);
+        }
+
+        else if(Input.GetMouseButtonUp(0))
+        {
+            Vector3 mousePt = camera.ScreenToWorldPoint(Input.mousePosition);
+
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            //if (Physics.Raycast(ray, out hit))
+            //{
+
+            selectedFollower.position = new Vector3(mousePt.x, liftHeight, mousePt.z);
+        }
+
+        //else if (Input.GetMouseButtonDown(0))
+        //{
+        //    LiftFollower();
+        //}
+    }
+
+    private void LiftFollower()
+    {
+        
+    }
 }
