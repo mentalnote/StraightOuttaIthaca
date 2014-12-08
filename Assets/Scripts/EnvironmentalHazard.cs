7c7353bf58509070d6fc3bccd36d857979354e88
Luke <lukeh-b@hotmail.com>
2014-12-09 03:56:04 +1000

    Added environmental hazard colliders and script

---
+++ Assets/Scripts/EnvironmentalHazard.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnvironmentalHazard : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        var damageScript = other.gameObject.GetComponent<DamageScript>();
+        Follower followerScript = other.gameObject.GetComponent<Follower>();
+        if (damageScript != null && followerScript != null && followerScript.FollowerState == Follower.State.GettingBlown)
+        {
+            damageScript.applyDamage(DamageScript.DamageType.PHYSICAL, 100);
+        }
+        
+        
+    }
+
+}
